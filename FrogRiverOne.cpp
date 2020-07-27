8ee9c094c42d9601f54d731fcf240d75726340a4
DongChyeon <kimdonghyun90766@gmail.com>
2020-07-27 22:10:38 +0900

    Update FrogRiverOne.cpp

---
+++ FrogRiverOne.cpp
@@ -0,0 +1,11 @@
+int solution(int X, vector<int>& A) {
+	set<int> check;	// set에 중복 원소가 허용 안된다는 점을 이용
+
+	for (int i = 0; i < A.size(); i++) {
+		if (A[i] <= X) {
+			check.insert(A[i]);
+			if (check.size() == X) return i;
+		}
+	}
+	return -1;
+}
