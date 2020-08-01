e6637a78fb888aa4d51cbdd3189a7a651b1fc871
DongChyeon <kimdonghyun90766@gmail.com>
2020-08-01 17:45:54 +0900

    Update PermCheck.cpp

---
+++ PermCheck.cpp
@@ -0,0 +1,22 @@
+#include <set>
+
+int solution(vector<int>& A) {
+	set<int> B;
+	int sum = 0;
+	int sum2 = 0;
+	int max = A[0];
+
+	for (int i = 0; i < A.size(); i++) {
+		B.insert(A[i]);
+	}
+	for (set<int>::iterator iter = B.begin(); iter != B.end(); iter++) {
+		sum += *iter;
+		if (*iter > max) max = *iter;
+	}
+	for (int i = 1; i < A.size() + 1; i++) {
+		sum2 += i;
+	}
+
+	if (sum == sum2) return 1;
+	else return 0;
+}
