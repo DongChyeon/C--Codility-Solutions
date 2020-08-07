353197eca60a7b6579bf09b824b97f8a33749481
DongChyeon <kimdonghyun90766@gmail.com>
2020-08-07 12:49:25 +0900

    Update Triangle.cpp

---
+++ Triangle.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+
+int solution(vector<int>& A) {
+	sort(A.begin(), A.end());
+	if (A.size() >= 3) {
+		for (int i = 0; i < A.size() - 2; i++) {
+			if (A[i + 2] < ((long long)A[i] + A[i + 1])) return 1;
+		}
+	}
+	return 0;
+}
