bb5e1290ed1843efd5cab3e2ce8123077a21743d
DongChyeon <kimdonghyun90766@gmail.com>
2020-08-17 13:24:56 +0900

    Update MaxProfit.cpp

---
+++ 9. Maximum slice problem/MaxProfit.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+
+int solution(vector<int>& A) {
+	if (A.size() < 2) return 0;
+	
+	int min_value = A[0];
+	int max_profit = 0;
+
+	for (int i = 0; i < A.size(); i++) {
+		min_value = min(min_value, A[i]);
+		max_profit = max(max_profit, A[i] - min_value);
+	}
+
+	return max_profit;
+}
